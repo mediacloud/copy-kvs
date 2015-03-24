e72dc3236cf56fbbd314b69eb3615ff3b2d38756
Linas Valiukas <shirshegsm@gmail.com>
2015-03-24 20:48:33 +0200

    Refactor the copying script to support multiple "connectors" (multiple GridFS, S3 access points); make the helper script use the argument "connectors" to do the copying

---
+++ bin/copy-kvs.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+
+use GridFSToS3;
+
+use YAML qw(LoadFile);
+
+sub main
+{
+	unless ($ARGV[2])
+	{
+		die "Usage: $0 config.yml from-connector to-connector";
+	}
+
+	my $config_file = $ARGV[0];
+	my $from_connector = $ARGV[1];
+	my $to_connector = $ARGV[2];
+
+	my $config;
+	eval {
+		$config = LoadFile($config_file);
+	};
+	if ( $@ ) {
+		die "Unable to read configuration from '$config_file': $@";
+	}
+
+    GridFSToS3::copy_kvs( $config, $from_connector, $to_connector );
+}
+
+main();
--- lib/GridFSToS3.pm
+++ lib/GridFSToS3.pm
@@ -19,151 +19,160 @@ use Parallel::Fork::BossWorkerAsync;
 use File::Slurp;
 use List::Util qw(max);
 
-# GridFS handlers (PID => $handler)
-my %_gridfs_handlers;
+# Storage handlers:
+# {
+#     'connector_name_1' => {
+#         $pid => $handler,
+#         ...
+#     },
+#     'connector_name_2' => {
+#         $pid => $handler,
+#         ...
+#     },
+#     ...
+# }
+my %_storage_handlers;
 
-# S3 handlers (PID => $handler)
-my %_s3_handlers;
 
 if ($0 =~ /\.inc\.pl/) {
-    die "Do not run this script directly.\n";
+    LOGDIE("Do not run this script directly.");
 }
 
 
-sub _gridfs_handler_for_pid($$)
+sub _new_storage_handler($$)
 {
-    my ($pid, $config) = @_;
+    my ( $config, $connector_name ) = @_;
 
-    unless (exists $_gridfs_handlers{$pid}) {
-        $_gridfs_handlers{$pid} = Storage::Handler::GridFS->new(
-            host => $config->{mongodb_gridfs}->{host} || 'localhost',
-            port => $config->{mongodb_gridfs}->{port} || 27017,
-            database => $config->{mongodb_gridfs}->{database},
-            timeout => int($config->{mongodb_gridfs}->{timeout}) || -1
+    my $handler = undef;
+
+    my $connector = $config->{ connectors }->{ $connector_name };
+    unless ( $connector ) {
+        LOGDIE("Connector '$connector_name' was not found.");
+    }
+
+    my $connector_type = $connector->{ type };
+    unless ( $connector_type ) {
+        LOGDIE("Connector type for connector '$connector_name' is not set.");
+    }
+
+    if ( lc( $connector_type ) eq lc( 'AmazonS3' ) ) {
+
+        $handler = Storage::Handler::AmazonS3->new(
+            access_key_id => $connector->{ access_key_id },
+            secret_access_key => $connector->{ secret_access_key },
+            bucket_name => $connector->{ bucket_name },
+            directory_name => $connector->{ directory_name } || '',
+            timeout => int( $connector->{ timeout } ) // 60,
+            use_ssl => $connector->{ use_ssl } // 0,
+            head_before_putting => $connector->{ head_before }->{ put } // 0,
+            head_before_getting => $connector->{ head_before }->{ get } // 0,
+            head_before_deleting => $connector->{ head_before }->{ delete } // 0,
+            overwrite => $connector->{overwrite} // 1,
         );
-        unless ($_gridfs_handlers{$pid}) {
-            LOGDIE("Unable to initialize GridFS handler for PID $pid");
-        }
-    }
 
-    if (scalar keys %_gridfs_handlers > 100) {
-        LOGDIE("Too many GridFS handlers initialized. Strange.");
-    }
+    } elsif ( lc( $connector_type ) eq lc( 'GridFS' )) {
 
-    return $_gridfs_handlers{$pid};
-}
-
-sub _s3_handler_for_pid($$)
-{
-    my ($pid, $config) = @_;
-
-    unless (exists $_s3_handlers{$pid}) {
-        $_s3_handlers{$pid} = Storage::Handler::AmazonS3->new(
-            access_key_id => $config->{amazon_s3}->{access_key_id},
-            secret_access_key => $config->{amazon_s3}->{secret_access_key},
-            bucket_name => $config->{amazon_s3}->{bucket_name},
-            directory_name => $config->{amazon_s3}->{directory_name} || '',
-            timeout => int($config->{amazon_s3}->{timeout}) // 60,
-            use_ssl => $config->{amazon_s3}->{use_ssl} // 0,
-            head_before_putting => $config->{amazon_s3}->{head_before}->{put} // 0,
-            head_before_getting => $config->{amazon_s3}->{head_before}->{get} // 0,
-            head_before_deleting => $config->{amazon_s3}->{head_before}->{delete} // 0,
-            overwrite => $config->{amazon_s3}->{overwrite} // 1,
+        $handler = Storage::Handler::GridFS->new(
+            host => $connector->{ host } || 'localhost',
+            port => $connector->{ port } || 27017,
+            database => $connector->{ database },
+            timeout => int( $connector->{ timeout } ) || -1
         );
-        unless ($_s3_handlers{$pid}) {
-            LOGDIE("Unable to initialize S3 handler for PID $pid");
+
+    } else {
+
+        LOGDIE("Unconfigured connector type '$connector_type'.");
+    }
+
+    return $handler;
+}
+
+sub _storage_handler_for_pid($$$)
+{
+    my ( $config, $connector_name, $pid ) = @_;
+
+    unless (exists $_storage_handlers{ $connector_name }{ $pid }) {
+
+        my $handler = _new_storage_handler( $config, $connector_name );
+        unless ($handler) {
+            LOGDIE("Unable to initialize storage handler '$connector_name' for PID $pid");
         }
+
+        $_storage_handlers{ $connector_name }{ $pid } = $handler;
     }
 
-    if (scalar keys %_s3_handlers > 100) {
-        LOGDIE("Too many S3 handlers initialized. Strange.");
+    if (scalar keys %_storage_handlers > 200) {
+        LOGDIE("Too many storage handlers initialized. Strange.");
     }
 
-    return $_s3_handlers{$pid};
+    return $_storage_handlers{ $connector_name }{ $pid };
 }
 
-sub _create_lock_file($)
+sub _read_last_copied_file($$)
 {
-    my $config = shift;
+    my ( $config, $connector_name ) = @_;
 
-    if (-e $config->{lock_file}) {
-        LOGDIE("Lock file '$config->{lock_file}' already exists.");
+    my $connector = $config->{ connectors }->{ $connector_name };
+    unless ( $connector ) {
+        LOGDIE("Connector '$connector_name' was not found.");
     }
-    open LOCK, ">$config->{lock_file}";
-    print LOCK "$$";
-    close LOCK;
-}
 
-sub _unlink_lock_file($)
-{
-    my $config = shift;
-
-    unlink $config->{lock_file};
-}
-
-sub _log_last_copied_file_from_gridfs_to_s3($$)
-{
-    my ($config, $last_copied_filename) = @_;
-
-    if (defined $last_copied_filename) {
-        open LAST, ">$config->{file_with_last_filename_copied_from_gridfs_to_s3}";
-        print LAST $last_copied_filename;
-        close LAST;
+    my $last_copied_file = $connector->{ last_copied_file };
+    unless ( defined $last_copied_file ) {
+        LOGDIE("Last copied file for connector '$connector_name' is not defined.");
     }
-}
 
-sub _log_last_copied_file_from_s3_to_gridfs($$)
-{
-    my ($config, $last_copied_filename) = @_;
-
-    if (defined $last_copied_filename) {
-        open LAST, ">$config->{file_with_last_filename_copied_from_s3_to_gridfs}";
-        print LAST $last_copied_filename;
-        close LAST;
+    unless ( -e $last_copied_file ) {
+        return undef;
     }
+
+    my $offset_filename = read_file( $last_copied_file );
+    unless ( defined $offset_filename ) {
+        die "Last copied file is undefined (read from '$last_copied_file'";
+    }
+
+    chomp $offset_filename;
+    return $offset_filename
 }
 
-sub _upload_file_to_s3
+sub _write_last_copied_file($$$)
 {
-    my ($job) = @_;
+    my ( $config, $connector_name, $last_copied_filename ) = @_;
 
-    my $filename = $job->{filename};
-    my $config = $job->{config};
+    unless ( defined $last_copied_filename ) {
+        LOGDIE("Last copied filename (that has to be stored) is not defined.");
+    }
+
+    my $connector = $config->{ connectors }->{ $connector_name };
+    unless ( $connector ) {
+        LOGDIE("Connector '$connector_name' was not found.");
+    }
+
+    my $last_copied_file = $connector->{ last_copied_file };
+    unless ( defined $last_copied_file ) {
+        LOGDIE("Last copied file for connector '$connector_name' is not defined.");
+    }
+
+    write_file( $last_copied_file, $last_copied_filename );
+}
+
+sub _copy_file_between_connectors
+{
+    my ( $job ) = @_;
+
+    my $filename = $job->{ filename };
+    my $config = $job->{ config };
+    my $from_connector = $job->{ from_connector };
+    my $to_connector = $job->{ to_connector };
 
     eval {
 
         # Get storage handlers for current thread (PID)
-        my $gridfs = _gridfs_handler_for_pid($$, $config);
-        my $amazons3 = _s3_handler_for_pid($$, $config);
+        my $from_storage = _storage_handler_for_pid( $config, $from_connector, $$ );
+        my $to_storage = _storage_handler_for_pid( $config, $to_connector, $$ );
 
         INFO("Copying '$filename'...");
-        $amazons3->put($filename, $gridfs->get($filename));
-
-    };
-
-    if ( $@ )
-    {
-        LOGDIE("Job error occurred while copying '$filename': $@");
-    }
-
-    return { filename => $filename };
-}
-
-sub _download_file_to_gridfs
-{
-    my ($job) = @_;
-
-    my $filename = $job->{filename};
-    my $config = $job->{config};
-
-    eval {
-
-        # Get storage handlers for current thread (PID)
-        my $amazons3 = _s3_handler_for_pid($$, $config);
-        my $gridfs = _gridfs_handler_for_pid($$, $config);
-
-        INFO("Copying '$filename'...");
-        $gridfs->put($filename, $amazons3->get($filename));
+        $to_storage->put( $filename, $from_storage->get( $filename ));
 
     };
 
@@ -175,39 +184,65 @@ sub _download_file_to_gridfs
     return { filename => $filename };
 }
 
-sub copy_gridfs_to_s3($)
+sub _global_timeout($$$)
 {
-    my ($config) = @_;
+    my ( $config, $from_connector_name, $to_connector_name ) = @_;
 
-    # Create lock file
-    _create_lock_file($config);
+    my $from_connector = $config->{ connectors }->{ $from_connector_name }
+        or LOGDIE("'From' connector '$from_connector_name' was not found.");
 
-    # Read last copied filename
-    my $offset_filename;
-    if (-e $config->{file_with_last_filename_copied_from_gridfs_to_s3}) {
-        open LAST, "<$config->{file_with_last_filename_copied_from_gridfs_to_s3}";
-        $offset_filename = <LAST>;
-        chomp $offset_filename;
-        close LAST;
+    my $to_connector = $config->{ connectors }->{ $to_connector_name }
+        or LOGDIE("'To' connector '$to_connector_name' was not found.");
 
-        INFO("Will resume from '$offset_filename'.");
+    my $from_timeout = $from_connector->{ timeout }
+        or LOGDIE("Invalid 'from' connector timeout");
+    my $to_timeout = $to_connector->{ timeout }
+        or LOGDIE("Invalid 'to' connector timeout");
+
+    return ( $from_timeout == -1 ? 0 : max( $from_timeout, $to_timeout ) * 3 );
+}
+
+sub copy_kvs($$$)
+{
+    my ( $config, $from_connector_name, $to_connector_name ) = @_;
+
+    unless ( $config->{ connectors }->{ $from_connector_name } ) {
+        LOGDIE("The connector to copy from '$from_connector_name' is not configured.");
+    }
+    unless ( $config->{ connectors }->{ $to_connector_name } ) {
+        LOGDIE("The connector to copy to '$to_connector_name' is not configured.");
     }
 
-    my $worker_threads = $config->{worker_threads} or LOGDIE("Invalid number of worker threads ('worker_threads').");
-    my $job_chunk_size = $config->{job_chunk_size} or LOGDIE("Invalid number of jobs to enqueue at once ('job_chunk_size').");
-    my $gridfs_timeout = int($config->{mongodb_gridfs}->{timeout}) or LOGDIE("Invalid GridFS timeout (must be positive integer or -1 for no timeout");
-    my $s3_timeout = int($config->{amazon_s3}->{timeout}) or LOGDIE("Invalid S3 timeout (must be positive integer");
+    # Create lock file
+    if ( -e $config->{ lock_file } ) {
+        LOGDIE("Lock file '$config->{lock_file}' already exists.");
+    }
+    write_file( $config->{ lock_file }, "$$" );
+
+    # Read last copied filename
+    my $offset_filename = _read_last_copied_file( $config, $from_connector_name );
+    if ( defined $offset_filename ) {
+        INFO("Will resume from '$offset_filename'.");
+    } else {
+        INFO("Will start from beginning.");
+    }
+
+    my $worker_threads = $config->{ worker_threads }
+        or LOGDIE("Invalid number of worker threads ('worker_threads').");
+    my $job_chunk_size = $config->{ job_chunk_size }
+        or LOGDIE("Invalid number of jobs to enqueue at once ('job_chunk_size').");
 
     # Initialize worker manager
     my $bw = Parallel::Fork::BossWorkerAsync->new(
-        work_handler    => \&_upload_file_to_s3,
-        global_timeout  => ($gridfs_timeout == -1 ? 0 : max($gridfs_timeout, $s3_timeout) * 3),
-        worker_count => $worker_threads,
+        work_handler    => \&_copy_file_between_connectors,
+        global_timeout  => _global_timeout( $config, $from_connector_name, $to_connector_name ),
+        worker_count    => $worker_threads,
     );
 
     # Copy
-    my $gridfs = _gridfs_handler_for_pid($$, $config);
-    my $list_iterator = $gridfs->list_iterator($offset_filename);
+    my $from_storage = _storage_handler_for_pid( $config, $from_connector_name, $$ );
+
+    my $list_iterator = $from_storage->list_iterator( $offset_filename );
     my $have_files_left = 1;
     while ($have_files_left)
     {
@@ -221,12 +256,18 @@ sub copy_gridfs_to_s3($)
                 $filename = $f;
             } else {
                 # No filenames left to copy, leave $filename at the last filename copied
-                # so that _log_last_copied_file_from_gridfs_to_s3() can write that down
+                # so that _write_last_copied_file() can write that down
                 $have_files_left = 0;
                 last;
             }
             DEBUG("Enqueueing filename '$filename'");
-            $bw->add_work({filename => $filename, config => $config});
+            my $job = {
+                filename => $filename,
+                config => $config,
+                from_connector => $from_connector_name,
+                to_connector => $to_connector_name,
+            };
+            $bw->add_work( $job );
         }
 
         while ($bw->pending()) {
@@ -240,97 +281,14 @@ sub copy_gridfs_to_s3($)
 
         # Store the last filename from the chunk as the last copied
         if ($filename) {
-            _log_last_copied_file_from_gridfs_to_s3( $config, $filename );
+            _write_last_copied_file( $config, $from_connector_name, $filename );
         }
     }
 
     $bw->shut_down();
 
     # Remove lock file
-    _unlink_lock_file($config);
-
-    INFO("Done.");
-
-    return 1;
-}
-
-sub copy_s3_to_gridfs($)
-{
-    my ($config) = @_;
-
-    # Create lock file
-    _create_lock_file($config);
-
-    # Read last copied filename
-    my $offset_filename;
-    if (-e $config->{file_with_last_filename_copied_from_s3_to_gridfs}) {
-        open LAST, "<$config->{file_with_last_filename_copied_from_s3_to_gridfs}";
-        $offset_filename = <LAST>;
-        chomp $offset_filename;
-        close LAST;
-
-        INFO("Will resume from '$offset_filename'.");
-    }
-
-    my $worker_threads = $config->{worker_threads}
-        or LOGDIE("Invalid number of worker threads ('worker_threads').");
-    my $job_chunk_size = $config->{job_chunk_size}
-        or LOGDIE("Invalid number of jobs to enqueue at once ('job_chunk_size').");
-    my $gridfs_timeout = int($config->{mongodb_gridfs}->{timeout})
-        or LOGDIE("Invalid GridFS timeout (must be positive integer or -1 for no timeout");
-    my $s3_timeout = int($config->{amazon_s3}->{timeout})
-        or LOGDIE("Invalid S3 timeout (must be positive integer");
-
-    # Initialize worker manager
-    my $bw = Parallel::Fork::BossWorkerAsync->new(
-        work_handler    => \&_download_file_to_gridfs,
-        global_timeout  => ($gridfs_timeout == -1 ? 0 : max($gridfs_timeout, $s3_timeout) * 3),
-        worker_count => $worker_threads,
-    );
-
-    # Copy
-    my $amazons3 = _s3_handler_for_pid($$, $config);
-    my $list_iterator = $amazons3->list_iterator($offset_filename);
-    my $have_files_left = 1;
-    while ($have_files_left)
-    {
-        my $filename;
-
-        for (my $x = 0; $x < $job_chunk_size; ++$x)
-        {
-            my $f = $list_iterator->next();
-            if ($f) {
-                # Filename to copy
-                $filename = $f;
-            } else {
-                # No filenames left to copy, leave $filename at the last filename copied
-                # so that _log_last_copied_file_from_s3_to_gridfs() can write that down
-                $have_files_left = 0;
-                last;
-            }
-            DEBUG("Enqueueing filename '$filename'");
-            $bw->add_work({filename => $filename, config => $config});
-        }
-
-        while ($bw->pending()) {
-            my $ref = $bw->get_result();
-            if ($ref->{ERROR}) {
-                LOGDIE("Job error: $ref->{ERROR}");
-            } else {
-                DEBUG("Restored file '$ref->{filename}'");
-            }
-        }
-
-        # Store the last filename from the chunk as the last copied
-        if ($filename) {
-            _log_last_copied_file_from_s3_to_gridfs($config, $filename);
-        }
-    }
-
-    $bw->shut_down();
-
-    # Remove lock file
-    _unlink_lock_file($config);
+    unlink $config->{lock_file};
 
     INFO("Done.");
 
